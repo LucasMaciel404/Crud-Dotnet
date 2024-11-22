7f5c81802765a5adb2811013e2c40f9dd662ca17
Lucas Maciel <maciellucas487@gmail.com>
2024-11-22 15:48:39 -0300

    feat: adicionado comunicacao/conexao ao banco de dados

--- Data/UserContext.cs
+++ Data/UserContext.cs
@@ -1,13 +1,12 @@
 ﻿using Crud.Model;
 using Microsoft.EntityFrameworkCore;
 
-namespace Crud.Data
+namespace Crud.Data;
+
+public class UserContext : DbContext
 {
-    public class UserContext : DbContext
-    {
-        public UserContext(DbContextOptions<UserContext> opts) : base(opts) {
-        
-        }
-        public DbSet<UserModel> Users { get; set; }
+    public UserContext(DbContextOptions<UserContext> opts) : base(opts) {
+    
     }
+    public DbSet<UserModel> Users { get; set; }
 }
