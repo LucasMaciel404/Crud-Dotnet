e4a80c3b3db16326646862722bdfc8dd667a58d6
Lucas Maciel <maciellucas487@gmail.com>
2024-11-22 06:23:29 -0300

    Adicionar arquivos de projeto.

---
+++ Data/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Crud.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> opts) : base(opts) {
+        
+        }
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ Model/USerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud.Model;
+public class UserModel
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    [Required]
+    public bool Ativo { get; set; }
+}
+
